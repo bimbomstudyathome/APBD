70300a1bc410d36e51f733d67fbefb3abccd45b5
s28464 <s28464@pjwstk.edu.pl>
2024-03-12 14:41:37 +0100

    Static avg method

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -4,3 +4,14 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World1!");
 Console.WriteLine("Hello, World2!");
 Console.WriteLine("Hello, World3!");
+avgMethod(new []{1,2,3,4,5});
+
+static void avgMethod(int[] arr)
+{
+    int sum = 0;
+    foreach (var num in arr)
+    {
+        sum += num;
+    }
+    Console.WriteLine("Average is: " + (sum/arr.Length));
+}
