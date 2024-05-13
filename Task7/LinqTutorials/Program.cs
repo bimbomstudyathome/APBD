2400ed1826f26237aac4f586e9a4aa54e5123264
mstitel-0 <jarik012005@gmail.com>
2024-05-13 15:54:00 +0200

    Task7 done

---
+++ Task7/LinqTutorials/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using LinqTutorials.Models;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = LinqTasks.Task15();
+            foreach (var res in t)
+            {
+                Console.WriteLine(res);
+            }
+            //Console.WriteLine(LinqTasks.Task13());
+        }
+    }
+}
